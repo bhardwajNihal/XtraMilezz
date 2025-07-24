17a2dded338dc5e331e9347d688b4e5052a67728
Nihal Bhardwaj <bhardwajnihal00721@gmail.com>
2025-07-24 05:55:58 +0530

    server action to submit assessment, generate Ai improvement tip

--- actions/quiz.ts
+++ actions/quiz.ts
@@ -65,3 +65,109 @@ export async function generateQuiz() {
   }
 
 }
+
+interface questionType {
+    question: string;
+    options: string[];
+    correctAnswer: string;
+    explanation: string
+}
+
+export async function assessQuiz(questions:questionType[], answers:string[]) {
+  
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    throw new Error("Request unauthorized!");
+  }
+
+  // check if user exists
+  const foundUser = await DbClient.user.findUnique({
+    where: {
+      email: session.user.email!,
+    },
+  });
+
+  if (!foundUser) {
+    throw new Error("User not found!");
+  }
+
+  // calculating quizScore
+  let quizScore:number = 0;
+
+  questions.map((ques,idx) => {
+    if(ques.correctAnswer == answers[idx]) quizScore = quizScore+1;
+  })
+
+  const quizResult = questions.map((ques,idx) => {
+
+    return {
+      question : ques.question,
+      answer : ques.correctAnswer,
+      userAnswer : answers[idx],
+      isCorrect : ques.correctAnswer == answers[idx],
+      explanation : ques.explanation
+    }
+  })
+  
+  // improvement tip to be ai generated
+  
+    // getting all the wrong answers
+    const wrongAnswers = quizResult.filter(q => q.isCorrect===false);
+
+    let improvementTip = null;
+
+    if(wrongAnswers.length>0){
+
+      const wrongAnsText = wrongAnswers.map(q => `
+        Question: ${q.question}\n Correct Answer: ${q.answer}\n User Answer: ${q.userAnswer}
+        `)
+        .join("\n\n");    // will join the array texts to a single string, adding newline
+
+    // creating prompt
+      const improvementPrompt = `
+      The user took a mock technical interview for the domain${foundUser.industry},
+      and got the following questions wrong:
+      ${wrongAnsText}.
+
+      Based on these mistakes, provide a concise, specific improvement tip.
+      Focus on the knowledge gaps revealed by these wrong answers.
+      Keep the response under 2 sentences and make it encouraging.
+      Don't explicitly mention the mistakes, instead focus on what to learn/practice.
+    `;
+    
+    // generate tip
+    try {
+      const tipGenerated = await model.generateContent(improvementPrompt);
+      improvementTip = tipGenerated.response.text().trim();
+
+    } catch (error) {
+      throw error;
+    }
+    }
+
+    // finally making entry to db
+    try {
+      const assessment = await DbClient.assessment.create({
+        data: {
+          userId : foundUser.id,
+          quizScore,
+          questions : quizResult,
+          category : "technical",
+          improvementTip
+        }
+      })
+
+      return assessment;
+      
+    } catch (error) {
+      throw error;
+    }
+
+
+  // userId
+  // quizScore      Float     //  quiz score
+  // questions      Json[]    // Array of {question, answer, userAnswer, isCorrect, explanation}
+  // category       String    // "Technical", "Behavioral", etc.
+  // improvementTip
+
+}
